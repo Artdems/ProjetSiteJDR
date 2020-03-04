0eef1f4615da6c127d46feb8a5f03a6eabac6567
Arthur DESMARAIS <adesmarais@amiltone.com>
2020-03-04 17:36:17 +0100

    ajoue de donner test dans la bd, l afichage fonctionne bien

--- ProjetPerso/DAL/DAL.cs
+++ ProjetPerso/DAL/DAL.cs
@@ -14,6 +14,11 @@ namespace ProjetPerso.DAL
 
         public List<Personnage> GetAllPersonnages()
         {
+            /*client.Add(new Personnage { Joueur = "Arthur", Pernoms = "Torgis", Nom = "Desmarais", Age = 23, Dexteriter = 0, Charisme = 1, Perseption = 2, Constitution = 3, Force = 4, Agiliter = 5, Vie = 20, Note = "Perso Teste" });
+            client.Add(new Personnage { Joueur = "Nicolas", Pernoms = "Daerios", Nom = "Perdrix", Age = 21, Dexteriter = 6, Charisme = 7, Perseption = 8, Constitution = 9, Force = 10, Agiliter = 11, Vie = 20, Note = "Perso Teste 2" });
+            client.Add(new Personnage { Joueur = "Jean", Pernoms = "Gatosec", Nom = "Rimlinger", Age = 24, Dexteriter = 12, Charisme = 13, Perseption = 14, Constitution = 15, Force = 16, Agiliter = 17, Vie = 20, Note = "Perso Teste 3" });
+            client.Add(new Personnage { Joueur = "Alexie", Pernoms = "Xélias", Nom = "Perrot", Age = 22, Dexteriter = 18, Charisme = 19, Perseption = 20, Constitution = 21, Force = 22, Agiliter = 23, Vie = 20, Note = "Perso Teste 4" });
+            client.SaveChanges();*/
             return (client.Personnages.ToList());
         }
 
