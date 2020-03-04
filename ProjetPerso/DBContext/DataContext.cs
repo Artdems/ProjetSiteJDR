4412ecdc227bb50f1425a51a0c29732be7b2daec
Arthur DESMARAIS <adesmarais@amiltone.com>
2020-03-04 14:01:22 +0100

    Creation de la solution

---
+++ ProjetPerso/DBContext/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetPerso.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetPerso.DBContext
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> option) : base(option) { }
+
+        public DbSet<Personnage> Personnages { get; set; }
+    }
+}
---
+++ ProjetPerso/DataBase/Personnage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetPerso.DataBase
+{
+    public class Personnage
+    {
+        public int ID { get; set; }
+        public string Joueur { get; set; }
+
+        public string Nom { get; set; }
+
+        public string Pernoms { get; set; }
+
+        public int Age { get; set; }
+
+        public int Dexteriter { get; set; }
+
+        public int Charisme { get; set; }
+        public int Perseption { get; set; }
+
+        public int Constitution { get; set; }
+
+        public int Force { get; set; }
+
+        public int Agiliter { get; set; }
+
+        public int Vie { get; set; }
+
+        public string Note { get; set; }
+
+    }
+}
