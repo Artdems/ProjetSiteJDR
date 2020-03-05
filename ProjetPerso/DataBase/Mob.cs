12b591d1db02ecc430cc0cca8097733c6ad85b1a
Arthur DESMARAIS <adesmarais@amiltone.com>
2020-03-05 17:21:36 +0100

    Ajout modal de confirmation pour la supression des donne Personnage

---
+++ ProjetPerso/DataBase/Mob.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetPerso.DataBase
+{
+    public class Mob
+    {
+        public int ID { get; set; }
+        public string Nom { get; set; }
+
+        public int Dexteriter { get; set; }
+
+        public int Charisme { get; set; }
+        public int Perseption { get; set; }
+
+        public int Constitution { get; set; }
+
+        public int Force { get; set; }
+
+        public int Agiliter { get; set; }
+
+        public int Vie { get; set; }
+
+        public string Note { get; set; }
+    }
+}
